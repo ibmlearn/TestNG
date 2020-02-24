7f54a8c5d1eae66056c66bb5fa7ece00672fa9d0
irshadbaig <irshadbaig.moghal@gmail.com>
2020-02-24 17:13:11 +0530

    testng dependency version update

--- src/main/java/com/ibm/testng/RunningTestNGFactoryProgrammatically.java
+++ src/main/java/com/ibm/testng/RunningTestNGFactoryProgrammatically.java
@@ -2,18 +2,17 @@ package com.ibm.testng;
 
 import org.testng.TestNG;
 
-
 public class RunningTestNGFactoryProgrammatically {
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException{
 		// TODO Auto-generated method stub
 		TestNG testNG = new TestNG();
 		@SuppressWarnings("rawtypes")
 		Class[] arrayOfClasses = new Class[1];
-		arrayOfClasses[0] = FactoriesWithDataProvidersTest.class;
+		arrayOfClasses[0] = Class.forName("com.ibm.testng.FactoriesWithDataProvidersTest");
 		testNG.setTestClasses( arrayOfClasses );
 		testNG.run();
 	}
